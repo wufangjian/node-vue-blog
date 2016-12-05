b135da890724c97713d45ad9f663de01e29127ae
wufangjian <wufangjian@baidu.com>
2016-12-05 20:21:34 +0800

    update

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App'
+
+import Vuex form from 'vuex'
+import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
+
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App }
+})
